6f411b464d62125816f43b792819776593239003
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-22 14:45:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,14 +2,14 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   
-  if  number % 3 == 0
-    return "Fizz"
+  if  (number % 3 == 0) && (number % 5 == 0)
+    return "FizzBuzz"
     
   elsif number % 5 == 0 
     return "Buzz"
     
-  elsif (number % 3 == 0) && (number % 5 == 0)
-    return "FizzBuzz"
+  elsif number % 3 == 0
+    return "Fizz"
     
   else 
     return nil
