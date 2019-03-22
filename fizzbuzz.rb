f87c55daf31a2394d161f777d6160a35eace1c2e
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-22 14:43:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,13 @@
 def fizzbuzz(number)
   
   if  number % 3 == 0
-    return "Fizz"
+    puts "Fizz"
     
   elsif number % 5 == 0 
-    return "Buzz"
+    puts "Buzz"
     
   elsif (number % 3 == 0) && (number % 5 == 0)
-    return "FizzBuzz"
+    puts "FizzBuzz"
     
   else 
     return nil
